5f242e4bab784a429459bf0a65efdda7633c281a
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2022-09-03 11:56:00 -0500

    primer archivo

---
+++ A01024944-A01197705-A01383102_ActIntegradora1/main.cpp
@@ -0,0 +1,15 @@
+// Actividad Integradora 1
+
+// Jose Emilio Alvear Cantu - A01024944
+// Andrea Catalina Fernandez Mena - A01197750
+// Carlos Milano - A01383102
+
+/*
+
+--Instrucciones-- 
+
+
+
+
+
+*/
