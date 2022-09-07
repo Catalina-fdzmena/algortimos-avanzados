bd6491dbe08ef44eedb54bf2d2f2467841f3c29c
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2022-09-06 23:41:52 -0500

    part 2 and 3 done

--- A01024944-A01197705-A01383102_ActIntegradora1/main.cpp
+++ A01024944-A01197705-A01383102_ActIntegradora1/main.cpp
@@ -6,26 +6,6 @@
 
 /*
 
---Instrucciones-- 
-
-En equipos de máximo 3 personas (la entrega es individual), escribe un programa en C++ que lea 5 archivos de texto (de nombre fijo, no se piden al usuario) que contienen exclusivamente caracteres del 0 al 9, caracteres entre A y F y saltos de línea
-     transmission1.txt
-     transmission2.txt
-     mcode1.txt
-     mcode2.txt
-     mcode3.txt
-
-Los archivos de transmisión contienen caracteres de texto que representan el envío de datos de un dispositivo a otro.
-Los archivos mcodeX.txt representan código malicioso que se puede encontrar dentro de una transmisión.
-
-El programa debe analizar si el contenido de los archivos mcode1.txt, mcode2.txt y mcode3.,txt están contenidos en los archivos transmission1.txt y transmission2.txt y desplegar un true o false si es que las secuencias de chars están contenidas o no. En caso de ser true, muestra true, seguido las posiciones (cero basadas) en el archivo de transmissiónX.txt donde inicia el código de mcodeY.txt separadas por un espacio. 
-
-Suponiendo que el código malicioso tiene siempre código "espejeado" (palíndromos de chars), sería buena idea buscar este tipo de código en una transmisión. El programa después debe buscar si hay código "espejeado" dentro de los archivos de transmisión. (palíndromo a nivel chars, no meterse a nivel bits),  muestra en una sola línea separados por un espacio dos enteros correspondientes a la posición (cero basadas) en donde inicia y termina el código "espejeado" más largo (palíndromo) para cada archivo de transmisión y dicho código malicioso. Puede asumirse que siempre se encontrará este tipo de código y es independiente de los códigos en los archivos mcodeX.txt.
-
-Finalmente el programa analiza qué tan similares son los archivos de transmisión, y debe mostrar (separados por un espacio) la posición inicial, la posición final (cero basadas) del primer archivo en donde se encuentra el substring más largo común entre ambos archivos de transmisión, así como dicho substring.
-
-Es indispensable respetar el formato de salida especificado a continuación.
-
 Input
      nada, solamente deben existir los 5 archivos en la misma ruta donde se ejecuta el programa    
 
@@ -43,8 +23,6 @@ Output
   Parte3
       posiciónInicial posiciónFinal (de substring común más largo entre archivos de transmisión 1 y 2) longitudSubstringComún substringComún
 
-
-
 */
 
 #include <iostream>
@@ -55,39 +33,232 @@ Output
 
 using namespace std;
 
-#define MAX 1001
+#define MAX 3003
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/* PART 1*/
+//PARTE 1 ACTIVIDA INTEGRADORA 
 
-// auxiliary function for KMP
-// Complexity O(n)
-vector<int> lpsvFunc(string patron){
-    int n = patron.length();
-    vector<int> lpsv(n, 0);             // longest proper prefix lo ponemos como sufijo
-    int j=0, i=1;
-    while (i < n){
-        if (patron[i] == patron[j]){    //si coinciden ambos patrones
-            lpsv[i] = j+1;
-            i++;
-            j++;
+//Output must be generated
+/*
+    (true | false) si es que el archivo transmission1.txt contiene el código (secuencia de chars) contenido en el archivo mcode1.txt, un espacio y la posición    
+     (true | false) si es que el archivo transmission2.txt contiene el código (secuencia de chars) contenido en el archivo mcode1.txt, un espacio y la posición
+     (true | false) si es que el archivo transmission1.txt contiene el código (secuencia de chars) contenido en el archivo mcode2.txt, un espacio y la posición
+     (true | false) si es que el archivo transmission2.txt contiene el código (secuencia de chars) contenido en el archivo mcode2.txt, un espacio y la posición
+     (true | false) si es que el archivo transmission1.txt contiene el código (secuencia de chars) contenido en el archivo mcode3.txt, un espacio y la posición
+     (true | false) si es que el archivo transmission2.txt contiene el código (secuencia de chars) contenido en el archivo mcode3.txt, un espacio y la posición
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//PARTE 2 ACT INTEGRADORA 
+//------------------------------ENCONTRAR CÓDIGO CON MALWARE -----------------------------------------------
+
+//----------------------------Utilizamos manacher para identificr fragmentos de código espejeados 
+
+// Complejidad: O(n)
+
+pair<string, int> manacher(string text){
+	string T = "";                                  // new aux string
+	int n = text.length();
+	for (int i=0; i<n; i++){
+		T += "|";                                   // adding "|" between each charatcter
+		T += text[i];
+	}
+	T += "|";
+	n = T.length();                                 // new length
+	vector<int> L(n);
+	L[0] = 0;
+	L[1] = 1;
+	int maxLong=0, maxCenter=0;
+	bool exp;
+	for (int c=1, li=0, ri=2; ri<n; ri++){
+		li = c-(ri-c);
+		exp = false;
+		if (c-maxLong <= ri && ri >= c+maxLong){
+			if (L[li] < (c+L[c]-ri)){
+				L[ri] = L[li];
+			}
+			else if(L[li] == (c + L[c]) - ri && (c + L[c]) == 2*n){
+				L[ri] = L[li];
+			}
+			else if(L[li] == (c + L[c]) - ri && (c + L[c]) < 2*n){
+				L[ri] = L[li];
+				exp = true;
+			}
+			else if(L[li] > (c + L[c]) - ri && (c + L[c]) < 2*n){
+				L[ri] = (c+L[c]) - ri;
+				exp = true;
+			}
+		}
+		else{
+			L[ri] = 0;
+			exp = true;
+		}
+		if (exp){
+			while((ri + L[ri] < n) && (ri - L[ri] > 0) && (T[ri - L[ri] - 1] == T[ri + L[ri] + 1])){
+				L[ri]++;
+			}
+		}
+		c = ri;
+		if (L[ri] > maxLong){
+	    	maxLong = L[ri];
+	    	maxCenter = ri;
+	    }
+	}
+	int start = (maxCenter - maxLong) / 2;
+	string exit = "";
+	for (int i=start; i<(start+maxLong); i++){
+		exit+=text[i];
+	}
+	return {exit, start};      // REGRESA PALINDROME Y POSICIÓN 
+    //missing this structure: 
+
+    //posiciónInicial posiciónFinal (para archivo de transmisión2) códigoMalicioso
+}
+
+//---------------------ACUMULAR CÓDIGO MALICIOSO -------------
+//Malware se encuentra a manera de palindromes
+
+// Complejidad: O(n)
+void findPalindrome(ofstream &outputResult, vector<string> &transmissionN){
+    vector< pair<string, int> > palindromes;
+    for (int i=0; i<transmissionN.size(); i++){                
+        palindromes.push_back(manacher(transmissionN[i]));
+    }
+
+    if (outputResult.is_open()){
+        outputResult << "Palíndromo más grande:" << endl;
+        for (int i=0; i<transmissionN.size(); i++){
+            outputResult << "\tTransmision" << i+1 << ".txt" << " ==> " << "Posición: " << palindromes[i].second << endl;
+            outputResult << "\t" << palindromes[i].first << endl;
+            outputResult << "\t----" << endl;
         }
-        else{                           // Si no coincide
-            if (j == 0){                // if it's at the start
-                lpsv[i] = 0;
-                i++;
+        outputResult << "\n==============\n" << endl;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//PARTE 3 ACT INTEGRADORA
+
+//--------------------------ENCONTRAR EL Longest Common Substring -------------------------------------- 
+
+// Complejidad: O(n*m)
+string LCSS(string st1, string st2){
+    int longestCommonSS[MAX][MAX];
+    int position = 0, length = 0;           
+
+    // first column
+    for (int i=0; i<st1.length(); i++){
+        if (st1[i] == st2[0]){
+            longestCommonSS[i][0] = 1;
+            // length = 1;
+        }
+        else{
+            longestCommonSS[i][0] = 0;
+        }
+    }
+
+//Revisar primer fila
+    for (int j=0; j<st2.length(); j++){
+        if (st1[0] == st2[j]){
+            longestCommonSS[0][j] = 1;
+            length = 1;
+        }
+        else{
+            longestCommonSS[0][j] = 0;
+        }
+    }
+
+//Continuar revisando txt para resto de valores 
+    for (int i=1; i<st1.length(); i++){
+        for (int j=1; j<st2.length(); j++){
+            if (st1[i] == st2[j]){
+                longestCommonSS[i][j] = longestCommonSS[i-1][j-1] + 1;
+                // length = (longestCommonSS[i][j] > length) ? longestCommonSS[i][j] : length;
+                if (longestCommonSS[i][j] > length){
+                    length += 1;                                            // update length if it gets surpassed
+                    position = i;                                           // store position
+                }
             }
-            else{                       // move one index back
-                j = lpsv[j-1];
+            else{
+                longestCommonSS[i][j] = 0;
             }
         }
     }
-    return lpsv;
+    return st1.substr(position-length+1, length);       // returns the substring from position=position-length+1 and of length=length
+}
+
+// Complejidad: O(1)
+void getLongestSubstring (ofstream &outputResult, vector<string> transmissionN){
+    if (outputResult.is_open()){
+        outputResult << "Substring más largo:" << endl;
+        
+        outputResult << "\tSubstring más largo t1-t2 ==> " << LCSS(transmissionN[0], transmissionN[1]) << endl;
+        outputResult << "\tSubstring más largo t1-t3 ==> " << LCSS(transmissionN[0], transmissionN[2]) << endl;
+        outputResult << "\tSubstring más largo t2-t3 ==> " << LCSS(transmissionN[1], transmissionN[2]) << endl;
+         
+        outputResult << "==============\n" << endl;
+    }
 }
 
 
-int main(){
+/* ----------------------------------------------------------------
+   ---------------------------------------------------------------- */
 
+int main(){
+    //Variables para transición y revisión de cada caso de prueba
+    string code1, code2, code3, t1, t2, t3;                        
+
+    ifstream codeDoc("mcode1.txt");
+    ifstream codeDoc2("mcode2.txt");
+    ifstream codeDoc3("mcode3.txt");                      // reading files
+    ifstream transmission1("transmission1.txt");
+    ifstream transmission2("transmission2.txt");
+    ifstream transmission3("transmission3.txt");
+    ofstream outputResult("outputResult.txt");
+
+    //Vector que almacena lo detectado como malware
+    vector<string> mcodeN;  //Defined with N at the end, cause it has N values
+
+    //Crear vector  para almacenar transmiciones                          
+    vector<string> transmissionN;   //Defined with N at the end, cause it has N values               
+
+/*
+    //Enviar código hacía MCNODE para almacenar aquel correspondiente
+    getline(codeDoc, code1);                     
+    getline(codeDoc2, code2);
+    getline(codeDoc3, code3);
+    //Enviar detecciones de malware a los vectores correspondientes
+    codeDoc.push_back(code1);
+    codeDoc2.push_back(code2);
+    codeDoc3.push_back(code3);*/
+
+//Asignar variables a malware según el archivo así como inclusión a su mismo vector
+
+    if (getline(codeDoc, code1)){
+        mcodeN.push_back(code1);
+    }else if(getline(codeDoc2, code2)){
+        mcodeN.push_back(code2);
+    }else if(getline(codeDoc3, code3)){
+        mcodeN.push_back(code3);
+    }else{
+        cout<<"Document is not availabe to read malware"<<endl;
+    }
+
+    //Asignar a cada una de las transmisiones una de las variables creadas
+    getline(transmission1, t1);                     
+    getline(transmission2, t2);
+    getline(transmission3, t3);
+
+    //Enviar las transmisiones acumuladas en las vaariables hacía el vector de transmisiones
+    transmissionN.push_back(t1);                    
+    transmissionN.push_back(t2);
+    transmissionN.push_back(t3);
+
+    //findCodes(outputResult, transmissionN, mcodeN);   //FOR PART 1
+    findPalindrome(outputResult, transmissionN);        //FOR PART 2
+    getLongestSubstring(outputResult, transmissionN);   //FOR PART 3
 
     return 0;
 }
