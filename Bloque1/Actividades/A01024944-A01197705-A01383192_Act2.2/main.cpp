3f32ddccc534af11464a5e19b495085a0cc77a82
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2022-09-07 01:47:44 -0500

    sufix array incompleto
    --falta revisar ingreso y conversión a char

---
+++ Bloque1/Actividades/A01024944-A01197705-A01383192_Act2.2/main.cpp
@@ -0,0 +1,117 @@
+/*
+--Actividad 2.2 Implementación de un Suffix Array--
+
+--Integrantes de Equipo:
+* Jose Emilio Alvear Cantu - A01024944
+* Andrea Catalina Fernandez Mena - A01197750
+* Carlos Milano - A01383102
+*/
+
+//Escribe un programa en C++ que dado un string, se calcule el arreglo de substrings y lo muestre ordenado alfabéticamente 
+
+//Algoritmo Suffix Array basado en algoritmo de: https://www.geeksforgeeks.org/suffix-array-set-2-a-nlognlogn-algorithm/ 
+
+
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+// Structure to store information of a suffix
+struct suffix
+{
+	int index; 
+	int level[2]; //Guardar organización en pares además del índice
+};
+
+
+int valuesComparisson(struct suffix a, struct suffix b)
+{
+	return (a.level[0] == b.level[0])? (a.level[1] < b.level[1] ?1: 0):
+			(a.level[0] < b.level[0] ?1: 0);
+}
+
+
+int *buildSuffixArray(char *txt, int n)
+{
+	// A structure to store suffixes and their indexes
+	struct suffix suffixes[n];
+
+	// Store suffixes and their indexes in an array of structures.
+	// The structure is needed to sort the suffixes alphabetically
+	// and maintain their old indexes while sorting
+	for (int i = 0; i < n; i++)
+	{
+		suffixes[i].index = i;
+		suffixes[i].level[0] = txt[i] - 'a';
+		suffixes[i].level[1] = ((i+1) < n)? (txt[i + 1] - 'a'): -1;
+	}
+
+	// Sort the suffixes using the comparison function
+	// defined above.
+	sort(suffixes, suffixes+n, valuesComparisson);
+	int suffixIndex[n]; 
+
+//Funcion para asignar nivel de jerarquía a identifiacar sufijos 
+
+	for (int k = 4; k < 2*n; k = k*2)
+	{
+		int level = 0;
+		int prev_level = suffixes[0].level[0];
+		suffixes[0].level[0] = level;
+		suffixIndex[suffixes[0].index] = 0;
+
+//Iteración para crear rank en los sufijos
+		for (int i = 1; i < n; i++)
+		{
+			if (suffixes[i].level[0] == prev_level &&
+					suffixes[i].level[1] == suffixes[i-1].level[1])
+			{
+				prev_level = suffixes[i].level[0];
+				suffixes[i].level[0] = level;
+			}
+			else 
+			{
+				prev_level = suffixes[i].level[0];
+				suffixes[i].level[0] = ++level;
+			}
+			suffixIndex[suffixes[i].index] = i;
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			int nextindex = suffixes[i].index + k/2;
+			suffixes[i].level[1] = (nextindex < n)?
+								suffixes[suffixIndex[nextindex]].level[0]: -1;
+		}
+
+		sort(suffixes, suffixes+n, valuesComparisson);
+	}
+
+	//Guardar el arreglo todos los prefijos trabajados
+
+	int *suffixArr = new int[n];
+	for (int i = 0; i < n; i++)
+		suffixArr[i] = suffixes[i].index;
+	return suffixArr;
+}
+
+
+void print(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+main(){
+    string wordChosen;
+    cin>>wordChosen;
+    char txt[] ="wordChosen";   //No se si sea la manera correcta de aceptar los casos de prueba
+	int n = strlen(txt);
+	int *suffixArr = buildSuffixArray(txt, n);
+	cout << "Following is suffix array for " << txt << endl;
+	print(suffixArr, n);
+	return 0;
+}
