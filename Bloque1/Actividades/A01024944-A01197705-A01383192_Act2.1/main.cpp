58b2e484c12b67a15aa66a5400d7a2c9af7d1e15
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2022-09-07 01:13:16 -0500

    hash string incomplete

---
+++ Bloque1/Actividades/A01024944-A01197705-A01383192_Act2.1/main.cpp
@@ -0,0 +1,38 @@
+/*
+--Actividad 2.1 Implementación de un Hash String--
+
+--Integrantes de Equipo:
+* Jose Emilio Alvear Cantu - A01024944
+* Andrea Catalina Fernandez Mena - A01197750
+* Carlos Milano - A01383102
+*/
+
+//Add preprocessors
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Falta implementas ASCII y lesctura dentro del mismo
+
+long long prhf(string S){
+    int n = S.length();
+    int p = 64;   //Ya que n es un entero múltiplo de 4 y  (16 <= n <=64)
+    int m = 1e9 + 9;
+    long long hashValue  = 0;
+    long long power = 1;
+    for (int i = 0; i < n ; i++){
+        //Definir como obtener el valor a partir de la hashing string
+        hashValue = (hashValue + (S[i] - 'a' + 1) * power) % m;
+        //Definir como obtener el valor de potencia para función hashing
+        power = (power * p) % m;
+    }
+    return hashValue;
+
+}
+
+main(){
+    int n {};
+    cin>>n;
+    long long prhf(string S);
+    return 0;
+}
