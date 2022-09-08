c8f44fabf1031939d86ec2adbb5ddf56f0988e06
Emilio0404 <emilio.alvear@hotmail.com>
2022-09-07 18:49:29 -0700

    se cambio de algoritmo a usar un map

--- Bloque1/Actividades/A01024944-A01197705-A01383192_Act2.2/main.cpp
+++ Bloque1/Actividades/A01024944-A01197705-A01383192_Act2.2/main.cpp
@@ -1,129 +1,64 @@
 /*
 --Actividad 2.2 Implementación de un Suffix Array--
-
 --Integrantes de Equipo:
-* Jose Emilio Alvear Cantu - A01024944
-* Andrea Catalina Fernandez Mena - A01197750
-* Carlos Milano - A01383102
+	* Jose Emilio Alvear Cantu - A01024944
+	* Andrea Catalina Fernandez Mena - A01197750
+	* Carlos Milano - A01383102
 */
 
-//Escribe un programa en C++ que dado un string, se calcule el arreglo de substrings y lo muestre ordenado alfabéticamente 
+// Escribe un programa en C++ que dado un string, se calcule el arreglo de substrings y lo muestre ordenado alfabéticamente 
 
-//Algoritmo Suffix Array basado en algoritmo de: https://www.geeksforgeeks.org/suffix-array-set-2-a-nlognlogn-algorithm/ 
+// Algoritmo Suffix Array basado en algoritmo metodo 2 de: https://www.geeksforgeeks.org/suffix-array-set-2-a-nlognlogn-algorithm/ 
 
-//Instrucciones: 
-//Ingresar desde bash de linux g++ -g main.cpp
-//Despues proceder a escribir ./a.out <text00.txt
+// Instrucciones: 
+// Ingresar desde bash de linux g++ -g main.cpp
+// Despues proceder a escribir ./a.out <text00.txt
 
-//Otra manera de imprimirlo sin mostrar la parte de la palabra restante es: 
-//Ingresar desde cmd y colocar la instrucción main.exe < text00.txt > nombreParaOutput.txt  
-      //Nota este método unicamente ejecuta las coordenas de órden alfabético
-
+// Otra manera de imprimirlo sin mostrar la parte de la palabra restante es: 
+// Ingresar desde cmd y colocar la instrucción main.exe < text00.txt > nombreParaOutput.txt  
+    // Nota este método unicamente ejecuta las coordenas de órden alfabético
 
 
 #include <iostream>
-#include <cstring>
+#include <map>
 #include <algorithm>
 using namespace std;
 
-// Structure to store information of a suffix
-struct suffix
+int main()
 {
-	int index; 
-	int level[2]; //Guardar organización en pares además del índice
-};
+	string palabra;
+	cin >> palabra;
+	int n = palabra.length();
+	map<string, int> Map;
+	int suffix[n];
 
+	string sub = "";
+	for (int i = n - 1; i >= 0; i--)
+	{
+		sub = palabra[i] + sub;
+		Map[sub] = i;
+	}
 
-int valuesComparisson(struct suffix a, struct suffix b)
-{
-	return (a.level[0] == b.level[0])? (a.level[1] < b.level[1] ?1: 0):
-			(a.level[0] < b.level[0] ?1: 0);
-}
+	int j = 0;
+	for (auto x : Map)
+	{
+		suffix[j] = x.second;
+		j++;
+	}
 
-
-int *buildSuffixArray(string txt, int n)
-{
-	// Struct para gurdar sufijos e indices
-	struct suffix suffixes[n];
-	// Organización de manera alfabética y revisar sus indices mientras se organizan.
+	cout << "El arreglo de sufijos para " << palabra << " es el siguiente:" << endl;
 	for (int i = 0; i < n; i++)
 	{
-		// Acomodar suffixesutilizando comparación
-		suffixes[i].index = i;
-		suffixes[i].level[0] = txt[i] - 'a';
-		suffixes[i].level[1] = ((i+1) < n)? (txt[i + 1] - 'a'): -1;
-	}
+		cout << suffix[i] << " ";
 
-	sort(suffixes, suffixes+n, valuesComparisson);
-	int suffixIndex[n]; 
-
-//Funcion para asignar nivel de jerarquía a identifiacar sufijos 
-
-	for (int k = 4; k < 2*n; k = k*2)
-	{
-		int level = 0;
-		int prev_level = suffixes[0].level[0];
-		suffixes[0].level[0] = level;
-		suffixIndex[suffixes[0].index] = 0;
-
-//Iteración para crear rank en los sufijos
-		for (int i = 1; i < n; i++)
+		int partialWord = suffix[i];
+		for (int j = partialWord; j < palabra.length(); j++)
 		{
-			if (suffixes[i].level[0] == prev_level &&
-					suffixes[i].level[1] == suffixes[i-1].level[1])
-			{
-				prev_level = suffixes[i].level[0];
-				suffixes[i].level[0] = level;
-			}
-			else 
-			{
-				prev_level = suffixes[i].level[0];
-				suffixes[i].level[0] = ++level;
-			}
-			suffixIndex[suffixes[i].index] = i;
+			cout << palabra[j] << " ";
 		}
 
-		for (int i = 0; i < n; i++)
-		{
-			int nextindex = suffixes[i].index + k/2;
-			suffixes[i].level[1] = (nextindex < n)?
-								suffixes[suffixIndex[nextindex]].level[0]: -1;
-		}
-
-		sort(suffixes, suffixes+n, valuesComparisson);
+		cout << endl;
 	}
 
-	//Guardar el arreglo todos los prefijos trabajados
-
-	int *suffixArr = new int[n];
-	for (int i = 0; i < n; i++)
-		suffixArr[i] = suffixes[i].index;
-	return suffixArr;
-}
-
-
-void print(int arr[], int n, string wordChosen)
-{
-	for (int i = 0; i < n; i++){
-		int partialWord = arr[i];
-		cout << arr[i] << " ";
-		//Segudo indice revisa el arreglo 
-		for(int j = partialWord; j<wordChosen.length(); j++){
-			cout << wordChosen[j] << " ";
-		}
-	
-	cout << endl;
-	}
-}
-
-int main(){
-    string wordChosen;
-    cin>>wordChosen;
-    // char txt[] ="wordChosen";   //No se si sea la manera correcta de aceptar los casos de prueba
-	// int n = strlen(txt);
-    int n = wordChosen.length();
-	int *suffixArr = buildSuffixArray(wordChosen, n);
-	cout << "El siguiente arreglo de sufijos en orden alfabético para " << wordChosen << " es:"<<endl;
-	print(suffixArr,n, wordChosen);
 	return 0;
 }
