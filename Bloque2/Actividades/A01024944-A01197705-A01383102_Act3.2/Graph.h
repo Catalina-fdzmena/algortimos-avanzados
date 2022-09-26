1a6b6a51a03a789d9fd197da205aa896ae4c0133
Emilio0404 <emilio.alvear@hotmail.com>
2022-09-26 14:44:05 -0500

    Se agregaron los archivos de la clase de grafos

---
+++ Bloque2/Actividades/A01024944-A01197705-A01383102_Act3.2/Graph.h
@@ -0,0 +1,66 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <iostream>
+#include <list>
+#
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <iostream>
+#include <list>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+class Graph {
+private:
+  int numNodes;
+  int numEdges;
+  // Lista de adyacencia
+  // vector de listas ligadas de pares (vertice, peso)
+  std::vector<std::list<std::pair<int, int>>> adjList;
+  
+  void split(std::string line, std::vector<int> &res);
+  
+public:
+  Graph();
+  ~Graph();
+  void readGraph(std::istream &input);
+  void print();
+  void DFS(int v);
+  void BFS(int v);
+};
+
+
+#endif // _GRAPH_H_
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+class Graph {
+private:
+  int numNodes;
+  int numEdges;
+  // Lista de adyacencia
+  // vector de listas ligadas de pares (vertice, peso)
+  std::vector<std::list<std::pair<int, int>>> adjList;
+
+  void split(std::string line, std::vector<int> &res);
+
+public:
+  Graph();
+  ~Graph();
+  void readGraph(std::istream &input);
+  void print();
+  void DFS(int v);
+  void BFS(int v);
+};
+
+#endif // _GRAPH_H_
---
+++ Bloque2/Actividades/A01024944-A01197705-A01383102_Act3.2/main.cpp
@@ -0,0 +1,33 @@
+/** 
+* Ejemplo que implementa Grafos dirigidos ponderados con una
+* Lista de adyacencia y los algoritmos DFS y BFS
+*
+* Compilacion para debug:  
+*    g++ -std=c++17 -g -o main *.cpp 
+* Ejecucion con valgrind:
+*    nix-env -iA nixpkgs.valgrind
+*    valgrind --leak-check=full ./main < TestCases/test01.txt
+*
+* Compilacion para ejecucion:  
+*    g++ -std=c++17 -O3 -o main *.cpp 
+* Ejecucion:
+*    ./main < TestCases/test01.txt
+**/
+
+#include <iostream>
+#include <sstream>
+#include "Graph.h"
+
+
+int main() {
+  std::cout << "Ejemplo de grafos (representacion y recorridos)!" << std::endl;
+  std::stringstream inputInfo;
+  inputInfo << std::cin.rdbuf(); 
+  // Construye un grafo a partir de la consola usando
+  // representacion de Lista de adyacencia
+  Graph g1;
+  g1.readGraph(inputInfo);
+  g1.print();
+  g1.DFS(1);
+  g1.BFS(1);
+}
