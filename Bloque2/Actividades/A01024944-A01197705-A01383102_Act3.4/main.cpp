5616078d19dc838c24f9199538986ae1218d8dd5
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2022-10-09 23:40:19 -0500

    main base

---
+++ Bloque2/Actividades/A01024944-A01197705-A01383102_Act3.4/main.cpp
@@ -0,0 +1,13 @@
+/*
+--Actividad 3.4--
+Grafos bipartitas
+
+--Integrantes de Equipo:
+	* Jose Emilio Alvear Cantu - A01024944
+	* Andrea Catalina Fernandez Mena - A01197705
+	* Carlos Milano - A01383102
+*/
+
+//Info recabada de: 
+ // https://www.geeksforgeeks.org/implement-min-heap-using-stl/
+
