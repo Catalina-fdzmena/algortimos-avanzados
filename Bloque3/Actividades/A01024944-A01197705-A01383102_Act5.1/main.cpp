c22b993c8fbc5efe6bfa6a2d13cd4f4b67ed65a5
CarlosMilano <a01383102@itesm.mx>
2022-11-29 01:18:08 -0600

    final5.1

--- Bloque3/Actividades/A01024944-A01197705-A01383102_Act5.1/main.cpp
+++ Bloque3/Actividades/A01024944-A01197705-A01383102_Act5.1/main.cpp
@@ -10,76 +10,104 @@
 ----Instrucciones ----
 **
 ** Compilacion para ejecucion:
-**    g++ -o main.exe main.cpp
+**    g++ -g main.exe main.cpp
 ** Ejemplo de ejecucion:
-**     
+**     ./a.out < test01.txt
 **    
 */
 
-#include<iostream>
-#include<vector>
-#include<stdio.h>
-#include<string.h>
-#include<sstream> 
-#define MOD 1000000007
+#include <vector>
+#include <utility>
+#include <iostream>
+#include <string>
+#include <regex>
+#include <algorithm>
+#include <sstream>
+
 using namespace std;
 
-vector<int> capTypes[105];
-vector<int> oneComb;
-vector<vector<int> > combinations; 
-int fullBit;
-
-int dp[1025][101];
-    
-long long int posibleCombHats(int mask, int i)
-{
-
-    if (mask == fullBit){
-      combinations.push_back(oneComb);
-      oneComb = vector<int>(0,0);
-      return 1;
-    } 
-    if (i > 100) return 0;
-    if (dp[mask][i] != -1) return dp[mask][i];
-  
-    long long int ways = posibleCombHats(mask, i+1);
-  
-    int size = capTypes[i].size();
-  
-    for (int j = 0; j < size; j++)
-    {
-        if (mask & (1 << capTypes[i][j])) continue;
-
-        vector<int> tmp = {i,j, mask | (1 << capTypes[i][j])};
-        oneComb.push_back(i);
-        ways += posibleCombHats(mask | (1 << capTypes[i][j]), i+1);
-        ways %= MOD;
-    }
-    return dp[mask][i] = ways;
+bool regexNumber(string numb) { 
+  const regex expReg("^[1-9]|10$");
+  return regex_match(numb, expReg);
 }
-  
-int main(){ 
-  int n;  
-  string temp, str;
-  cin >> n;
-  int x;    
-  getline(cin, str);  
-  for (int i=0; i<n; i++){
-    getline(cin, str);
-    stringstream ss(str);
-    while (ss >> temp){
-      stringstream s;
-      s << temp;
-      s >> x;
-      capTypes[x].push_back(i);
-    }
+
+bool regexHat(string numb) { 
+  const regex expReg("^([1-9]|[1-9][0-9]|100)$");
+  return regex_match(numb, expReg);
+}
+
+bool validHatChoice (vector<int> currentComb, int currentHat) {
+  for (auto hat : currentComb) {
+    if (hat == currentHat) return false;
+  }
+  return true;
+}
+
+//Complejidad: O(2^N)
+void findHatCombinations (int currentPerson, vector<vector<int> > hatsPerPerson, vector<bool> assignedHats, vector<int> currentCombination, vector<vector<int> > &hatsCombination) {
+  if (all_of(assignedHats.begin(), assignedHats.end(), [](int i){return i == 1;})) {
+    hatsCombination.push_back(currentCombination);
+    return;
   }
 
-  fullBit = (1 << n) - 1;
-  
+  for (int person = currentPerson; person < hatsPerPerson.size(); person++) {
+    if (!assignedHats[person]) { 
+      for (int hat = 0; hat < hatsPerPerson[person].size(); hat++) {
+          int currentHat = hatsPerPerson[person][hat];
+          if (validHatChoice(currentCombination, currentHat)) {
+            currentCombination.push_back(hatsPerPerson[person][hat]);
+            assignedHats[person] = true;
 
-  memset(dp, -1, sizeof dp);
-  int cnter =0;
-  cout << posibleCombHats(0, 1) << endl;
-  return 0;
-}
+            findHatCombinations(currentPerson + 1, hatsPerPerson, assignedHats, currentCombination, hatsCombination);
+
+            currentCombination.pop_back();
+            assignedHats[person] = false;
+          }
+      }
+    }
+  }
+};
+
+int main () {
+  string inputNumberPersons;
+  vector<vector<int> > hatsPerPerson;
+  vector<bool> assignedHats;
+  vector<vector<int> > hatsCombination;
+  vector<int> currentCombination;
+
+  cin >> inputNumberPersons;
+
+  cin.ignore();
+  for (int person = 0; person < stoi(inputNumberPersons); person++) {
+    string inputHat;
+    vector<int> hatsList;
+    getline(cin, inputHat);
+    
+    stringstream ss(inputHat);
+    for (string i; ss >> i;) {
+      if (regexHat(i)) {
+        hatsList.push_back(stoi(i));
+      } else {
+        exit(0);
+      }
+    }
+
+    hatsPerPerson.push_back(hatsList);
+    assignedHats.push_back(0);
+  }
+
+  findHatCombinations(0, hatsPerPerson, assignedHats, currentCombination, hatsCombination);
+
+  cout << hatsCombination.size() << endl;
+  for (int person = 0; person < hatsCombination.size(); person++) {
+    cout << "(";
+    for (int hat = 0; hat < hatsCombination[person].size(); hat++) {
+      cout << hatsCombination[person][hat];
+      if(hat < hatsCombination[person].size() - 1){
+        cout << ",";
+      }
+      else{}
+    }
+    cout << ")\n";
+  }
+};
